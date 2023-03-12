bc90d562cb20847d2855d2de6f1a44826eb1db88
SebastianTong <tongsebastian27@gmail.com>
2023-03-12 15:31:10 -0300

    added port 3k

--- controllers/register.js
+++ controllers/register.js
@@ -2,7 +2,7 @@ const handleRegister = (req, res, db, bcrypt) => {
   const {email, name, password} = req.body;
   if (!email || !name || !password) {
     return res.status(400).json('Incorrect form submission');
-  } else {
+  } 
   const hash = bcrypt.hashSync(password)
     db.transaction(trx => {
       trx.insert({
@@ -26,7 +26,7 @@ const handleRegister = (req, res, db, bcrypt) => {
       .catch(trx.rollback)
     })
   .catch(err => res.status(400).json('unable to register'))
-  }}
+  }
 
 module.exports = {
   handleRegister
