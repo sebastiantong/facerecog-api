62d61a2e01917e21ebb1de2e45221c26f02cfa1a
SebastianTong <tongsebastian27@gmail.com>
2023-03-12 18:01:55 -0300

    added response on root endpoint

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const signin = require('./controllers/signin')
 const profile = require('./controllers/profile')
 const image = require('./controllers/image')
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const DATABASE_URL = process.env.DATABASE_URL;
 
 const db = knex({     //connecting the db to our server with knex. DATABASE_URL declared in env vars.
@@ -24,14 +24,14 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  // res.send(database.users) fake database no longer exists
+  res.send('hello')
 })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
-app.listen(PORT || 3000, ()=> {    //hosts on port 3k and second parameter is a function that runs after the server is up.
+app.listen(PORT, ()=> {    //hosts on port 3k and second parameter is a function that runs after the server is up.
   console.log(`app OK in ${PORT}`)
 })
 
